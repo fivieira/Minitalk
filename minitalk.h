347df7d1d1f6b76998a23893c4e263fae1367d02
Filipe Fernandes Vieira Sousa <fivieira@c1r3s2.42porto.com>
2023-08-14 14:42:43 +0100

    commit minitalk

---
+++ minitalk.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fivieira <fivieira@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/12 11:26:48 by fivieira          #+#    #+#             */
+/*   Updated: 2023/08/14 14:37:02 by fivieira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include "../ft_printf/libft/libft.h"
+# include "../ft_printf/ft_printf.h"
+
+#endif
